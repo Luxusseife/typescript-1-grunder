8714b173f702e8aca6243939b02c6ce8ce982f00
Jenny Lind <jeli2308@student.miun.se>
2024-03-30 21:27:30 +0100

    Ändrat style på knappar, ändrat style på containers samt lagt till mediaquery för större skärmar i base/layout.scss, lagt till en kontroll av kurskod och en funktion som uppdaterar kurs utifrån kurskod.

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,11 +1,11 @@
 // Deklarerar variabler.
-const saveCourseEl = document.getElementById("saveCourse") as HTMLButtonElement;
-const resetFormEl = document.getElementById("resetForm") as HTMLButtonElement;
-const listContainerEl = document.getElementById("list-container") as HTMLDivElement;
-const clearCoursesEl = document.getElementById("clear") as HTMLButtonElement;
+const saveCourseEl: HTMLButtonElement = document.getElementById("saveCourse") as HTMLButtonElement;
+const resetFormEl: HTMLButtonElement = document.getElementById("resetForm") as HTMLButtonElement;
+const listContainerEl: HTMLDivElement = document.getElementById("list-container") as HTMLDivElement;
+const clearCoursesEl: HTMLButtonElement = document.getElementById("clear") as HTMLButtonElement;
 
 // Definierar egenskaper hos ett objekt.
-interface courseInfo {
+interface CourseInfo {
     code: string;
     name: string;
     progression: string;
@@ -20,11 +20,11 @@ clearCoursesEl.addEventListener("click", clearCourses, false);
 // Funktion som hämtar sparade kurser i localStorage vid sidladdning.
 window.onload = () => {
     // Hämtar information från localStorage.
-    const savedCourses = localStorage.getItem('courses');
+    const savedCourses: string | null = localStorage.getItem('courses');
 
     // Kontrollerar om det finns sparade kurser.
     if (savedCourses) { 
-        const courses: courseInfo[] = JSON.parse(savedCourses);
+        const courses: CourseInfo[] = JSON.parse(savedCourses);
         courses.forEach(course => {
             addCourse(course);
         });
@@ -36,11 +36,11 @@ function saveCourse(): void {
     // Lagrar input från fälten för namn, kod, progression och url.
     const nameInput: string = (document.getElementById("courseName") as HTMLInputElement).value;
     const codeInput: string = (document.getElementById("courseCode") as HTMLInputElement).value;
-    const progressionInput: string = (document.querySelector('input[name="progression"]:checked') as HTMLInputElement).value;
+    const progressionInput: string = (document.querySelector('input[name="progression"]:checked') as HTMLInputElement)?.value;
     const syllabusInput: string = (document.getElementById("courseSyllabus") as HTMLInputElement).value;
 
     // Lagrar kurs i interfacen.
-    const newCourse: courseInfo = {
+    const newCourse: CourseInfo = {
         name: nameInput,
         code: codeInput,
         progression: progressionInput,
@@ -49,9 +49,34 @@ function saveCourse(): void {
 
     // Kontrollerar att alla kursfält är ifyllda.
     if (codeInput.length === 0 || nameInput.length === 0 || syllabusInput.length === 0) {
+        // Skickar ett meddelande till användaren.
         alert("Alla fält behöver fyllas i!");
+        return;
+    }
 
-    // Kör en utskrifts-funktion, rensar formulär och sparar till localStorage.
+    // Kontrollerar om kurskoden redan är tillagd.
+    const doesCourseCodeExist = document.querySelector(`[data-code="${codeInput}"]`);
+    if (doesCourseCodeExist) {
+        // Skickar ett meddelande till användaren. 
+        if (confirm("Denna kurs redan lagts till. Vill du uppdatera kursinformationen?")) {
+            // Skriver över tidigare information med ny.
+            doesCourseCodeExist.innerHTML = `
+                <ul>
+                    <li>Kursnamn: ${newCourse.name}</li>
+                    <li>Kurskod: ${newCourse.code}</li>
+                    <li>Progression: ${newCourse.progression}</li>
+                    <li>Kursplan: <a href=${newCourse.syllabus}>Länk!</a></li>
+                </ul>
+            `;
+
+            // Rensar formuläret.
+            (document.getElementById("coursesForm") as HTMLFormElement).reset();
+
+            // Sparar den uppdaterade kurslistan till localStorage.
+            updateListToStorage(newCourse);
+        }
+    
+    // Om kursen inte är tillagd, kör en utskrifts-funktion, sparar till localStorage och rensar formulär.
     } else {
         // Utskriftsfunktion.
         addCourse(newCourse);
@@ -65,11 +90,13 @@ function saveCourse(): void {
 }
 
 // Funktion som skriver ut kurs i DOM.
-function addCourse(newCourse: courseInfo) : void {
+function addCourse(newCourse: CourseInfo) : void {
     // Skapar ett nytt div-element.
-    const newListItemEl = document.createElement("div") as HTMLDivElement;
+    const newListItemEl: HTMLDivElement = document.createElement("div");
     // Sätter ett ID på elementet.
     newListItemEl.setAttribute("id", `list`);
+    // Sätter ett anpassat attribut på elementet.
+    newListItemEl.setAttribute("data-code", newCourse.code);
     // Skriver ut till DOM.
     newListItemEl.innerHTML += `
         <ul>
@@ -85,19 +112,50 @@ function addCourse(newCourse: courseInfo) : void {
 }
 
 // Funktion som sparar/uppdaterar kurslistan i localStorage.
-function updateListToStorage(newCourse: courseInfo): void {
+function updateListToStorage(newCourse: CourseInfo): void {
     // Läser in sparade kurser från localStorage om sådana finns, annars skapas en tom array.
-    let courses: courseInfo[] = JSON.parse(localStorage.getItem("courses") || "[]");
+    let courses: CourseInfo[] = JSON.parse(localStorage.getItem("courses") || "[]");
 
-    // Lägger till ny kurs i listan.
-    courses.push(newCourse);
+    // Lokaliserar index för den kurs som ska uppdateras.
+    const index: number = courses.findIndex(course => course.code === newCourse.code);
+
+    // Villkorssats; om kursen finns i listan - uppdatera informationen, annars - lägg till ny kurs.
+    if (index !== -1) { 
+        courses[index] = newCourse;
+    } else {
+        courses.push(newCourse);
+    }
 
     // Sparar den uppdaterade kurslistan till localStorage.
     localStorage.setItem("courses", JSON.stringify(courses));
 }
 
+
+// Funktion som uppdaterar en kurs utifrån angiven kurskod.
+function updateCourse(updatedCourse: CourseInfo): void {
+    // Hämtar array med sparade kurser.
+    const jsonCourses: string | null = localStorage.getItem('items');
+    if (jsonCourses) {
+        // Konverterar strängen till en array.
+        let courses: CourseInfo[] = JSON.parse(jsonCourses);
+
+        // Hittar den tidigare sparade kursen och uppdatera informationen.
+        courses = courses.map(course => {
+            if (course.code === updatedCourse.code) {
+                // Returnerar den uppdaterade kursen.
+                return updatedCourse;
+            }
+            // Returnerar kursen utan uppdatering.
+            return course;
+        });
+
+        // Sparar och skickar den uppdaterade arrayen till localStorage.
+        localStorage.setItem('items', JSON.stringify(courses));
+    }
+}
+
 // Funktion som rensar kurslistan och sparade kurser i localStorage.
-function clearCourses() {
+function clearCourses(): void {
     // Rensar kurslistan.
     listContainerEl.innerHTML = "";
     // Raderar sparade poster i localStorage.
